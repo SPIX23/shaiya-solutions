ed8cf70c96c27a1ea2de5ea5799938bcc79613c8
Bowie <61043863+426f776965@users.noreply.github.com>
2020-05-08 04:51:44 -0400

    Update remove apostrophes.sql

--- remove apostrophes.sql
+++ remove apostrophes.sql
@@ -1,5 +1,5 @@
 use ps_gamedefs
---removes apostrophes from these columns to prevent errors
+--removes apostrophes from these columns to help prevent failed log inserts
 update dbo.skills set skillname=replace(skillname,'''','');
 update dbo.mobs set mobname=replace(mobname,'''','');
 update dbo.items set itemname=replace(itemname,'''','');
