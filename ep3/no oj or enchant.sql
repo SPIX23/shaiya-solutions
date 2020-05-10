86dfcc5b64397c87a050b141ce8a0fd7ce12074d
Bowie <61043863+426f776965@users.noreply.github.com>
2020-05-10 04:35:24 -0400

    Add files via upload

---
+++ ep3/no oj or enchant.sql
@@ -0,0 +1,3 @@
+use ps_gamedefs update items set reqluc = 0 where reqluc <> 0
+use ps_gamedefs update items set server = 0 where server <> 0
+use ps_gamedefs update items set reqwis = 0 where reqwis <> 0
